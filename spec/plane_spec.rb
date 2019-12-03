8c2065e619b575d6c65ac4845fef76d3948ea363
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-03 11:25:24 +0000

    removing useless tests and cleaning up code with rubocop

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,49 +1,20 @@
 require 'plane'
-require 'airport'
-require 'weather_spec'
 
 describe Plane do 
 
   let(:plane) { Plane.new }
-  let(:airport) { Airport.new}
-
-  it "is in the airport after landing" do
-    airport.land(plane)
-    expect(airport.hanger).to include plane
-  end
-
-  it "should initialize with no planes airborn" do
-    expect(plane.airborn).to eq(false)
-  end
-
-  it "shouldn't be airborn when in the hanger" do
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.hanger = [plane]
-    expect(plane.airborn).to be_falsey
-  end
-
-  it "should be airborn if it takes off successfully from the airport" do
-    allow(airport).to receive(:stormy?).and_return(false)
-    airport.land(plane)
-    airport.take_off(plane)
-    expect(plane.airborn).to be_truthy
-  end
 
   describe '#in_the_air' do
-
-    it "should be airborn when in the air is called" do 
+    it "should be in the air after take off" do
       plane.in_the_air
-      expect(plane.airborn).to be_truthy
+      expect(plane.airborn?).to eq true
     end
-
-  end 
+  end
 
   describe '#landed' do
-
-    it "should not be airborn when landed is called" do 
+    it "should be landed after landing" do
       plane.landed
-      expect(plane.airborn).to be_falsey
+      expect(plane.airborn?).to eq false
     end
-
   end
 end
